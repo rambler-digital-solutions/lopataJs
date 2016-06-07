710486fb73a348064039297fbcd570e5073c5b54
Koshkarov Ruslan <jumangi@inbox.ru>
2016-06-07 14:03:46 +0300

    add some new methods; package; todo; gitignore (#1)
    * add some new methods; package; todo; gitignore
    
    * eslint rambler upd;

---
+++ TODO.md
@@ -0,0 +1,2 @@
+1) Event listener (methods like .on() .off(), bind)
+2) Polyfills (ex. for CustomEvent)
--- index.js
+++ index.js
@@ -40,10 +40,83 @@ module.exports = {
     var i;
     for (i = 0; i < array.length; i++) callback.call(scope, i, array[i]);
   },
+
+  extend: (obj1, obj2) => {
+    let extended = {};
+    let merge = (obj, cb) => {
+      let keys = Object.keys(obj);
+      let keysLength = keys.length;
+      let i = 0;
+      for (; i < keysLength; i++) {
+        let key = keys[i];
+        extended[key] = obj[key];
+      }
+      if (typeof cb === 'function') cb();
+    };
+    merge(obj1);
+    merge(obj2);
+    return extended;
+  },
+  isEmpty(obj) {
+    if (obj === null) return true;
+    if (obj && obj.length > 0) return false;
+    if (obj && obj.length === 0) return true;
+    for (let key in obj) {
+      if (hasOwnProperty.call(obj, key)) return false;
+    }
+
+    return true;
+  },
+  isElement(obj) {
+    try {
+      return obj instanceof HTMLElement;
+    } catch (e) {
+      return (typeof obj === 'object') &&
+        (obj.nodeType === 1) && (typeof obj.style === 'object') &&
+        (typeof obj.ownerDocument === 'object');
+    }
+  },
+  inArray(needle, haystack) {
+    let length = haystack.length;
+    for (let i = 0; i < length; i++) {
+      if (haystack[i] === needle) return true;
+    }
+    return false;
+  },
+  updateQuery: (uri, key, value) => {
+    let re = new RegExp('([?&])' + key + '=.*?(&|$)', 'i');
+    let separator = uri.indexOf('?') !== -1 ? '&' : '?';
+    let res = '';
+    if (uri.match(re)) {
+      res = uri.replace(re, '$1' + key + '=' + value + '$2');
+    } else {
+      res = uri + separator + key + '=' + value;
+    }
+    return res;
+  },
+  ajax: (url, success, failure) => {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+      // TODO: check data type!
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // TODO: make cross browser implementation
+        let response = document.implementation.createHTMLDocument('');
+        response.body.innerHTML = xhr.responseText;
+        if (typeof success === 'function') success(response);
+      } else if (xhr.readyState === 4) {
+        if (typeof failure === 'function') failure();
+      }
+    };
+    xhr.open('GET', url, true);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.send();
+  },
+
   jsonpRequest(url) {
     let callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
     let script = document.createElement('script');
     script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
     document.body.appendChild(script);
   }
+
 };
