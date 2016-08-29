0eca3997f2c941dbb177d9610dca7d8faca3c40e
Brodetsky Yan <y.brodetsky@rambler-co.ru>
2016-08-29 19:35:10 +0300

    A little clearing

--- index.js
+++ index.js
@@ -1,54 +1,52 @@
 module.exports = {
-  getCookie(name) {
+  getCookie: function(name) {
     let matches = document.cookie.match(new RegExp(
       '(?:^|; )' + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + '=([^;]*)'
     ));
     return matches ? decodeURIComponent(matches[1]) : undefined;
   },
-  setCookie: (name, _value, _props) => {
-    let props = _props || {};
-    let exp = props.expires;
-    let d;
-    let value = encodeURIComponent(_value);
-    let updatedCookie;
-    let propName;
-    let propValue;
+  setCookie: function(name, value, props) {
+    var _props = props || {};
+    var exp = _props.expires;
+    var d;
+    var _value = encodeURIComponent(value);
+    var updatedCookie;
+    var propName;
+    var propValue;
     if (typeof exp === 'number' && exp) {
       d = new Date();
       d.setTime(d.getTime() + exp * 1000);
-      exp = props.expires = d;
+      exp = _props.expires = d;
     }
-    if (exp && exp.toUTCString) props.expires = exp.toUTCString();
-    updatedCookie = name + '=' + value;
-    for (propName in props) {
-      if (!props.hasOwnProperty(propName)) continue;
+    if (exp && exp.toUTCString) _props.expires = exp.toUTCString();
+    updatedCookie = name + '=' + _value;
+    for (propName in _props) {
+      if (!_props.hasOwnProperty(propName)) continue;
       updatedCookie += '; ' + propName;
-      propValue = props[propName];
+      propValue = _props[propName];
       if (propValue !== true) updatedCookie += '=' + propValue;
     }
     document.cookie = updatedCookie;
   },
-  outerHeight(el, margins) {
-    let height = el.offsetHeight;
-    let style;
+  outerHeight: function (el, margins) {
+    var height = el.offsetHeight;
+    var style;
     if (!margins) return height;
     style = getComputedStyle(el);
     height += parseInt(style.marginTop, 10) + parseInt(style.marginBottom, 10);
     return height;
   },
-  forEach(array, callback, scope) {
-    var i;
-    for (i = 0; i < array.length; i++) callback.call(scope, i, array[i]);
+  forEach: function(array, callback, scope) {
+    for (var i = 0; i < array.length; i++) callback.call(scope, i, array[i]);
   },
-
-  extend: (obj1, obj2) => {
-    let extended = {};
-    let merge = (obj, cb) => {
-      let keys = Object.keys(obj);
-      let keysLength = keys.length;
-      let i = 0;
+  extend: function(obj1, obj2) {
+    var extended = {};
+    var merge = function(obj, cb) {
+      var keys = Object.keys(obj);
+      var keysLength = keys.length;
+      var i = 0;
       for (; i < keysLength; i++) {
-        let key = keys[i];
+        var key = keys[i];
         extended[key] = obj[key];
       }
       if (typeof cb === 'function') cb();
@@ -57,17 +55,17 @@ module.exports = {
     merge(obj2);
     return extended;
   },
-  isEmpty(obj) {
+  isEmpty: function(obj) {
     if (obj === null) return true;
     if (obj && obj.length > 0) return false;
     if (obj && obj.length === 0) return true;
-    for (let key in obj) {
+    for (var key in obj) {
       if (hasOwnProperty.call(obj, key)) return false;
     }
 
     return true;
   },
-  isElement(obj) {
+  isElement: function(obj) {
     try {
       return obj instanceof HTMLElement;
     } catch (e) {
@@ -76,17 +74,17 @@ module.exports = {
         (typeof obj.ownerDocument === 'object');
     }
   },
-  inArray(needle, haystack) {
-    let length = haystack.length;
-    for (let i = 0; i < length; i++) {
+  inArray: function(needle, haystack) {
+    var length = haystack.length;
+    for (var i = 0; i < length; i++) {
       if (haystack[i] === needle) return true;
     }
     return false;
   },
-  updateQuery: (uri, key, value) => {
-    let re = new RegExp('([?&])' + key + '=.*?(&|$)', 'i');
-    let separator = uri.indexOf('?') !== -1 ? '&' : '?';
-    let res = '';
+  updateQuery: function(uri, key, value) {
+    var re = new RegExp('([?&])' + key + '=.*?(&|$)', 'i');
+    var separator = uri.indexOf('?') !== -1 ? '&' : '?';
+    var res = '';
     if (uri.match(re)) {
       res = uri.replace(re, '$1' + key + '=' + value + '$2');
     } else {
@@ -94,13 +92,13 @@ module.exports = {
     }
     return res;
   },
-  ajax: (url, success, failure) => {
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
+  ajax: function (url, success, failure) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
       // TODO: check data type!
       if (xhr.readyState === 4 && xhr.status === 200) {
         // TODO: make cross browser implementation
-        let response = document.implementation.createHTMLDocument('');
+        var response = document.implementation.createHTMLDocument('');
         response.body.innerHTML = xhr.responseText;
         if (typeof success === 'function') success(response);
       } else if (xhr.readyState === 4) {
@@ -111,12 +109,16 @@ module.exports = {
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     xhr.send();
   },
+  jsonpRequest: function(url, callback) {
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+    window[callbackName] = function(data) {
+      delete window[callbackName];
+      document.body.removeChild(script);
+      callback(data);
+    };
 
-  jsonpRequest(url) {
-    let callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
-    let script = document.createElement('script');
+    var script = document.createElement('script');
     script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
     document.body.appendChild(script);
   }
-
 };
